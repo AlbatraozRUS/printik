6a6f6e1609e159b7844ec427d878561af2c0a5f8
AlbatraozRUS <55453060+AlbatraozRUS@users.noreply.github.com>
2020-04-07 19:24:11 +0300

    My own version of printf()

---
+++ printik.s
@@ -0,0 +1,337 @@
+global _printik
+section .text
+
+_printik:
+  pop r14
+  mov r15, rbp
+
+  push r9
+  push r8
+  push rcx
+  push rdx
+  push rsi
+  push rdi
+  mov rbp, rsp
+
+  mov r10, rdi
+  mov r12, 8
+
+  repeat:
+  mov rax, '%'
+  call strchr
+
+  cmp r10, 0
+  je simple_string
+
+who_is_who:
+
+  mov r9, r10
+  sub r9, rdi
+
+  push rdi
+  push rsi
+  mov rax, 1
+  mov rsi, rdi
+  mov rdi, 1
+  mov rdx , r9
+  syscall
+  pop rsi
+  pop rdi
+
+  inc r10
+
+  mov rbx, [r10]
+
+  mov al, 'c'
+  cmp al, bl
+  je chr
+
+  mov al, 's'
+  cmp al, bl
+  je str
+
+  mov al, 'd'
+  cmp al, bl
+  je dec
+
+  mov al, 'b'
+  cmp al, bl
+  je bin
+
+  mov al, 'h'
+  cmp al, bl
+  je hex
+
+  mov al, '%'
+  cmp al, bl
+  je proc
+  
+;_______________________________________________________________________________
+
+chr:
+
+  mov rax, [rbp + r12]
+  add r12, 8
+  mov r9, buf
+  mov [r9], rax
+  mov rax, 1
+  mov rsi, buf
+  mov rdi, 1
+  mov rdx, 1
+  syscall
+
+  inc r10
+  mov rdi, r10
+
+  jmp repeat
+
+;_______________________________________________________________________________
+
+proc:
+
+  mov rax,'%'
+  mov r9, buf
+  mov [r9], rax
+  mov rax, 1
+  mov rsi, buf
+  mov rdi, 1
+  mov rdx, 1
+  syscall
+
+  inc r10
+  mov rdi, r10
+
+  jmp repeat
+
+;_______________________________________________________________________________
+
+dec:
+    xor rcx, rcx
+    mov rax, [rbp + r12]
+    add r12, 8
+
+
+    mov rbx, 10
+  rep_dec:
+    xor rdx, rdx
+    div rbx
+    cmp rax, rdx
+    je check_dec
+    back_dec:
+    add rdx, '0'
+
+    mov r9, buf
+    add r9, rcx
+    mov [r9], rdx
+    inc rcx
+    jmp rep_dec
+
+    check_dec:
+    cmp rax, 0
+    jne back_dec
+
+    end_dec:
+
+    rep_2_dec:
+    mov rax, 1
+    mov rsi, buf
+    add rsi, rcx
+    dec rsi
+    mov rdi, 1
+    mov rdx, 1
+    push rcx
+    syscall
+    pop rcx
+    dec rcx
+    cmp rcx, 0
+    jne rep_2_dec
+
+    inc r10
+    mov rdi, r10
+
+    jmp repeat
+
+;_______________________________________________________________________________
+
+hex:
+  xor rcx, rcx
+  mov rax, [rbp + r12]
+  add r12, 8
+
+
+  rep_hex:
+  mov rbx, 16
+  xor rdx, rdx
+  div rbx
+  cmp rax, rdx
+  je check_hex
+  back_hex:
+  add rdx, num16
+
+  mov r9, buf
+  add r9, rcx
+  mov rbx, [rdx]
+  mov [r9], bl
+  inc rcx
+  jmp rep_hex
+
+  check_hex:
+  cmp rax, 0
+  jne back_hex
+
+  end_hex:
+
+  rep_2_hex:
+  mov rax, 1
+  mov rsi, buf
+  add rsi, rcx
+  dec rsi
+  mov rdi, 1
+  mov rdx, 1
+  push rcx
+  syscall
+  pop rcx
+  dec rcx
+  cmp rcx, 0
+  jne rep_2_hex
+
+  inc r10
+  mov rdi, r10
+
+  jmp repeat
+
+;_______________________________________________________________________________
+
+bin:
+      mov rax, [rbp + r12]
+      add r12, 8
+      mov rbx, rax
+      mov r9, 63
+    rep_bin:
+      mov rbx, rax
+      mov rcx, 64
+      sub rcx, r9
+      cmp r9, 0
+      je end_bin
+      rol rbx, cl
+      and rbx, 0x0000001
+      cmp rbx, 0
+      je zero
+      jmp one
+
+    zero:
+      push rax
+      push rcx
+      mov rax, 1
+      mov rsi, num16
+      mov rdi, 1
+      mov rdx, 1
+      syscall
+      pop rcx
+      pop rax
+      dec r9
+      jmp rep_bin
+
+    one:
+      push rax
+      push rcx
+      mov rax, 1
+      mov rsi, num16
+      inc rsi
+      mov rdi, 1
+      mov rdx, 1
+      syscall
+      pop rcx
+      pop rax
+      dec r11
+      jmp rep_bin
+
+    end_bin:
+      inc r10
+      mov rdi, r10
+
+      jmp repeat
+
+;_______________________________________________________________________________
+
+str:
+    mov rsi, [rbp + r12]
+    add r12, 8
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 1
+
+  rep_str:
+    syscall
+    inc rsi
+    mov rbx, [rsi]
+    cmp bl, 0
+    jne rep_str
+
+    inc r10
+    mov rdi, r10
+
+    jmp repeat
+
+;_______________________________________________________________________________
+
+simple_string:
+  mov rax, 1
+  mov rsi, rdi
+  mov rdi, 1
+  mov rdx, 1
+rep_sim_str:
+  syscall
+  inc rsi
+  cmp byte [rsi], 0
+  jne rep_sim_str
+  jmp ex
+
+;_______________________________________________________________________________
+
+global strchr
+strchr:
+  push rbx
+  again:
+  mov bl, [r10]
+
+  cmp bl, al
+  je found
+
+  cmp bl, 0
+  je not_found
+
+  inc r10
+  jmp again
+
+  found:
+  pop rbx
+  ret
+
+  not_found:
+  xor r10,r10
+  pop rbx
+  ret
+
+;_______________________________________________________________________________
+
+ex:
+  pop rax
+  pop rax
+  pop rax
+  pop rax
+  pop rax
+  pop rax
+  mov rbp, r15
+  push r14
+
+  ret
+;_______________________________________________________________________________
+section .data
+
+num16: db '0123456789ABCDEF'
+;_______________________________________________________________________________
+section .bss
+
+buf: resb 64
+bufSize: equ $ - buf
